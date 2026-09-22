85f7bfcd0abda9a7ed8d45fcf9e3a9d65cd8675b
rafanvieira86 <rafanvieira86@gmail.com>
2026-09-22 15:24:13 -0300

    Create pctt_trf2.js

---
+++ pctt_trf2.js
@@ -0,0 +1,14 @@
+const pcttDB = {
+  "00.01.01.01": ["3", "", "Guarda Permanente"],
+  "00.01.01.03": ["3", "", "Guarda Permanente"],
+  "00.01.01.05": ["3", "", "Guarda Permanente"],
+  "00.01.01.07": ["3", "", "Guarda Permanente"],
+  "00.01.01.09": ["3", "", "Guarda Permanente"],
+  "00.01.01.11": ["Enquanto vigora", "", "Eliminação"],
+  "00.01.01.12": ["10", "5", "Guarda Permanente"],
+  "00.01.01.13": ["10", "5", "Guarda Permanente"],
+  "00.01.01.14": ["2", "", "Eliminação"],
+  "00.01.01.15": ["3", "", "Guarda Permanente"],
+  "00.01.01.16": ["3", "", "Guarda Permanente"],
+  "00.01.01.17": ["Enquanto vigente", "3", "Guarda Permanente"]
+};
